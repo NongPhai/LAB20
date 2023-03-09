d2eb22551c2b5fe74f126ad468c8121bd40a90bf
Phai <norapat_c@cmu.ac.th>
2023-03-09 22:17:22 +0700

    completed

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,10 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect r1,Rect r2){
+	double w2,h2;
+	w2=min(r1.x+r1.w,r2.x+r2.w)-max(r1.x,r2.x);
+	h2=min(r1.y,r2.y)-max(r1.y-r1.h,r2.y-r2.h);
+	if(w2>0 && h2>0) return w2*h2;
+	else return 0; 
 }
